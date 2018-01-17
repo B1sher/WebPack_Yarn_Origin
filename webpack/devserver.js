7cc49279cef271c5888a28d871808603f09aca8d
B1sher <taraninaen@mail.ryazan.ru>
2018-01-18 00:33:45 +0300

    Revert "Первая настройка 1.0"
    This reverts commit 4238979ec873d8614a3e64dbf1391998c3a35f55.

--- webpack/devserver.js
+++
@@ -1,8 +0,0 @@
-module.exports = function() {
-    return {
-        devServer: {
-            stats: 'errors-only', // Отображает лишь ошибки в терминале
-            port: 9000 // Порт
-        }
-    };
-};
